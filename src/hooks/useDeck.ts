af8155876c817717d76c3a184b6f619d53c14700
Andreas Kjellqvist <andreas.kjellqvist@timeedit.com>
2023-10-28 23:53:00 +0200

    Persist deck to LocalStorage

--- src/hooks/useDeck.ts
+++ src/hooks/useDeck.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { Card } from '../types/Card';
+import { persist } from 'zustand/middleware';
 
 type DeckStore = {
     deck: Card[];
@@ -8,38 +9,45 @@ type DeckStore = {
     clearDeck: () => void;
 };
 
-const useDeck = create<DeckStore>()((set) => ({
-    deck: [],
-    addCard: (card: Card) =>
-        set((state: DeckStore) => {
-            //Checking if deck is full
-            if (state.deck.length >= 40) return {};
+const useDeck = create<DeckStore>()(
+    persist(
+        (set) => ({
+            deck: [],
+            addCard: (card: Card) =>
+                set((state: DeckStore) => {
+                    //Checking if deck is full
+                    if (state.deck.length >= 40) return {};
 
-            //Checking if card reached max
-            if (
-                state.deck.filter((unit) => unit.name === card.name).length >=
-                card.limit
-            )
-                return {};
+                    //Checking if card reached max
+                    if (
+                        state.deck.filter((unit) => unit.name === card.name)
+                            .length >= card.limit
+                    )
+                        return {};
 
-            //Add card
-            return { deck: [...state.deck, card] };
+                    //Add card
+                    return { deck: [...state.deck, card] };
+                }),
+            removeCard: (card: Card) =>
+                set((state: DeckStore) => {
+                    //Getting index to remove
+                    const indexToRemove = state.deck.indexOf(card);
+
+                    //Avoiding mutations
+                    const newArray = [...state.deck];
+
+                    //Removing card
+                    newArray.splice(indexToRemove, 1);
+
+                    //Setting new deck
+                    return { deck: newArray };
+                }),
+            clearDeck: () => set({ deck: [] }),
         }),
-    removeCard: (card: Card) =>
-        set((state: DeckStore) => {
-            //Getting index to remove
-            const indexToRemove = state.deck.indexOf(card);
-
-            //Avoiding mutations
-            const newArray = [...state.deck];
-
-            //Removing card
-            newArray.splice(indexToRemove, 1);
-
-            //Setting new deck
-            return { deck: newArray };
-        }),
-    clearDeck: () => set({ deck: [] }),
-}));
+        {
+            name: 'decked-out-card-builder-storage',
+        }
+    )
+);
 
 export default useDeck;
